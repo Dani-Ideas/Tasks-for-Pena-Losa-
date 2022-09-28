5ad7cc4e5ec213bf613a8f1d28cfa4154c337c8c
Dani-Ideas <d7n456.17@gmail.com>
2022-09-28 15:10:36 -0500

    function not functioabavileble

--- encargos/tercerEncargo/CYP03.c
+++ encargos/tercerEncargo/CYP03.c
@@ -1,73 +1,68 @@
 #include <stdio.h>
 
-int day, month, year;
-
-int main()
+int treeDesition(int *day, int *month,int *year)
 {
-    scanf("%d %d %d",&day,&month,&year);
-    if (month>=1&&month<13&&day>0&&year<=3000)
-    {
-        if (month ==12||month ==10||month ==8||month ==7||month ==5||month ==3||month ==1)
+    if (*month ==12||*month ==10||*month ==8||*month ==7||*month ==5||*month ==3||*month ==1)
         {
-            if (day == 31)
+            if (*day == 31)
             {
-                day=1;
-                if (month==12)
+                if (*month==12)
                 {
-                    year++,month=1;
-                    printf("%2d %2d %4d",day, month,year);
+                    return*day=1,*year++,*month=1;
                 }
                 else
                 {
-                    month++;
-                    printf("%2d %2d %4d",day, month,year);  
+                    return*day=1,*month++;
                 }
             }
-            else if (day==4&&month==10&&1582)
+            else if (*day==4&&*month==10&&*year==1582)
             {
-                day=15;
-                printf("%2d %2d %4d",day, month,year);
+                return*day=15;
             }
             else
             {
-                day++;
-                printf("%2d %2d %4d",day, month,year);
+                return*day++;
             }
         }
-        else if (month==4||month==9||month==6||month==11)
+        else if (*month==4||*month==9||*month==6||*month==11)
         {
-            if (day==30)
+            if (*day==30)
             {
-                day=1,month++;
-                printf("%2d %2d %4d",day, month,year);
+                return*day=1,*month++;
             }
             else
             {
-                day++;
-                printf("%2d %2d %4d",day, month,year);
+                return*day++;
             }
         }
         else
         {
-            if (day==28)
+            if (*day==28)
             {
-                if (year%400==0||year%4==0)
+                if (*year%400==0||*year%4==0)
                 {
-                    day++;
-                    printf("%2d %2d %4d",day, month,year);
+                    return*day++;
                 }
                 else
                 {
-                    day=1,month++;
-                    printf("%2d %2d %4d",day, month,year);
+                    return*day=1,*month++;
                 }
             }
             else
             {
-                day++,month++;
-                printf("%2d %2d %4d",day, month,year);
+                return*day++,*month++;
             }
         }   
+}
+
+int main()
+{
+    int day, month, year;
+    scanf("%d %d %d",&day,&month,&year);
+    if (month>=1&&month<13&&day>0&&year<=3000)
+    {
+        treeDesition(&day,&month,&year);
+        printf("%2d %2d %4d",day, month,year);
     }
     else
     {
