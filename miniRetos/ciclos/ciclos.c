a4a8068c04e8c17e804ba93ced5744ceb595cf99
Dani-Ideas <d7n456.17@gmail.com>
2022-11-05 13:02:25 -0600

    iterate the program until the user wants

--- miniRetos/ciclos/ciclos.c
+++ miniRetos/ciclos/ciclos.c
@@ -2,16 +2,22 @@
 //hacer un programa que imprima el signo "*" el número de veces que sea indicado
 void main(void)
 {
-    int iLimite;
-    int iContador;
-    printf("Limite\n");
-    scanf("%i", &iLimite);
-    iContador=0;
-
-    while (iContador < iLimite)
+    int iLimite, iContador, continuar=1;
+    
+    while (continuar)
     {
-        printf("*");
-        iContador= iContador +1;
+        iContador=0;
+        printf("Limite\n");
+        scanf("%i", &iLimite);
+        while (iContador < iLimite)
+        {
+            printf("*");
+            iContador= iContador +1;
+        }
+        printf("¿Se te ocurre otro numero?\nSí= cualquier tecla, No=0\n");
+        scanf("%i", &continuar);
     }
+    
+    
     printf("\n Fin\n");
 }
